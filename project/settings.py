21d6bc0a810b1dc61124939623f2d579b8280feb
shubhpy <shubhpycode@gmail.com>
2016-10-26 12:50:43 +0530

    Deleted myapp, Added urls and a html template

--- project/settings.py
+++ project/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'south',
-    'myapp',
     'markdown_deux',
     'blog',
 ]
